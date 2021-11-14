e427691807c98713a2afde0ee4446ce5c413b5e4
Abinesh S <abineshsiva191@gmail.com>
2021-11-14 14:01:24 +0530

    Modified robot text

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,6 @@ const contentfulConfig = {
     process.env.CONTENTFUL_DELIVERY_TOKEN,
 };
 
-// If you want to use the preview API please define
-// CONTENTFUL_HOST and CONTENTFUL_PREVIEW_ACCESS_TOKEN in your
-// environment config.
-//
-// CONTENTFUL_HOST should map to `preview.contentful.com`
-// CONTENTFUL_PREVIEW_ACCESS_TOKEN should map to your
-// Content Preview API token
-//
-// For more information around the Preview API check out the documentation at
-// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
-//
-// To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
 if (process.env.CONTENTFUL_HOST) {
   contentfulConfig.host = process.env.CONTENTFUL_HOST;
   contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
@@ -41,6 +29,22 @@ module.exports = {
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
+    {
+      resolve: "gatsby-remark-embed-video",
+      options: {
+          width: 800,
+          ratio: 1.77, 
+          height: 400, 
+          related: false,
+          noIframeBorder: true
+      },
+   },
+   {
+    resolve: `gatsby-remark-responsive-iframe`,
+    options: {
+      wrapperStyle: `margin-bottom: 1.0725rem`,
+    },
+  },
     "gatsby-transformer-remark",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sass",
@@ -50,6 +54,12 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: contentfulConfig,
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/about/`, `/blog/*`],
+      },
     },
     {
       resolve: `gatsby-plugin-google-adsense`,
@@ -67,22 +77,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-NNB4R6PH7X","637-917-9664",
-          // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
         gtagConfig: {
           send_page_view: true,
           cookie_expires: 0,
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
           respectDNT: true,
         },
       },
