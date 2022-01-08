6d0ce646e2b2d9e8c2e2fd60add68087291a444d
Abinesh S <abineshsiva191@gmail.com>
2022-01-08 20:14:35 +0530

    Purginh

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
+        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     },
     {
